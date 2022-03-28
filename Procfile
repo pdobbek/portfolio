3e2966971d078c0d82fb2ca8dba8dfb75b1affb3
pdobbek <p.dobbek@pm.me>
2022-03-28 18:49:42 +0100

    refactor: move all files from portfolio folder up

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python3 portfolio/manage.py runserver
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^p41_h=3ib(sfhhz0y5s2_biupy(!4j0u_h59=2v^iamzmd*mg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
