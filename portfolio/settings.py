20c840d508ff6dd99a4e4d7a1d811cd907c79adf
pdobbek <pdobbek@gmail.com>
2022-03-31 18:33:12 +0100

    fix static files bug on local server
    also fixed a typo in gitignore

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.joinpath('static')
 STATICFILES_DIRS = (
     BASE_DIR.joinpath('static'),
 )
+STATIC_ROOT = BASE_DIR.joinpath('/static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
